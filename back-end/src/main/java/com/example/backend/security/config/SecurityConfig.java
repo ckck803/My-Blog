1c2e5a3173a2c2a2b67807de496db775400b6bcb
Dongwoo <ckck803@gmail.com>
2022-01-13 02:02:06 +0900

    [back-end] : 설정 추가 및 변경
    - Application DB 설정 추가
    - Jpa Auditing 기능 추가
    - Spring Security 비활성화

--- back-end/src/main/java/com/example/backend/security/config/SecurityConfig.java
+++ back-end/src/main/java/com/example/backend/security/config/SecurityConfig.java
@@ -1,2 +1,24 @@
-package com.example.backend.security.config;public class SecurityConfig {
-}
+package com.example.backend.security.config;
+
+
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        super.configure(web);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .anyRequest().permitAll();
+//    }
+//}
