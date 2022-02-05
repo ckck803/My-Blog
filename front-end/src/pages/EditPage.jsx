4d731818efd63dbfa04b456f8516d4fee98d95f9
Dongwoo <ckck803@gmail.com>
2022-02-05 10:42:51 +0900

    [front-end] Editor 컴포넌트 분리

--- front-end/src/pages/EditPage.jsx
+++ front-end/src/pages/EditPage.jsx
@@ -15,84 +15,17 @@ import { useCallback } from "react";
 import ToastUIEditor from "../components/contents/ToastUIEditor";
 import { useMediaQuery } from "react-responsive";
 import { useParams } from "react-router-dom";
+import {
+  CategoryContainer,
+  CategoryLine,
+  InputBorder,
+  InputCategory,
+  InputTitle,
+  Select,
+  TitleContainer,
+} from "../assets/css/editor";
 
 dotenv.config();
-
-const TitleContainer = styled.div`
-  /* height: "15vh"; */
-
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  padding-right: 1.75vw;
-  padding-left: 1.75vw;
-  /* padding-right: 2rem;
-  padding-left: 2rem; */
-  height: 2.75rem; ;
-`;
-
-const CategoryContainer = styled.div`
-  padding-right: 2rem;
-  /* padding-top: 1rem;
-  padding-bottom: 1rem;
-  padding-right: 2rem;
-  padding-left: 2rem;
-  height: 2.75rem; ; */
-`;
-
-const InputBorder = styled.span`
-  border-left: 5px solid rgb(200, 206, 212);
-  /* line-height: 2em; */
-  /* height: 100%; */
-`;
-
-const InputTitle = styled.input`
-  padding-right: 5px;
-  padding-left: 10px;
-
-  border: 1px solid rgb(0, 0, 0, 0);
-  width: 900px;
-
-  font-size: 2.75rem;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-`;
-
-const InputCategory = styled.input`
-  padding: 0.2em 0.2em 0.2em 0.2em;
-
-  border: 1px solid rgb(0, 0, 0, 0);
-  width: 230px;
-  height: 100%;
-
-  font-size: 1.17rem;
-
-  &:active,
-  &:focus {
-    outline: none;
-  }
-`;
-
-const CategoryLine = styled.div`
-  width: 220px;
-  border-top: 1.5px solid rgb(200, 206, 212);
-`;
-
-const Select = styled.select`
- margin-right: 10px
-  content: "";
-  width: 170px; /* 원하는 너비설정 */
-  padding: 0.2em 0.2em 0.2em 0.2em; /* 여백으로 높이 설정 */
-  font-family: inherit; /* 폰트 상속 */
-  background: url(https://farm1.staticflickr.com/379/19928272501_4ef877c265_t.jpg)
-    no-repeat 95% 50%; /* 네이티브 화살표 대체 */
-  border: 1px solid #999;
-  border-radius: 1px; /* iOS 둥근모서리 제거 */
-  -webkit-appearance: none; /* 네이티브 외형 감추기 */
-  -moz-appearance: none;
-  appearance: none;
-`;
 
 const EditPage = () => {
   const { id } = useParams();
