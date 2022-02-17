b84110f9e2c096515b04c560475e5ea5f67c61e9
Dongwoo <ckck803@gmail.com>
2022-02-17 22:37:41 +0900

    [front-end] Front End 파일 분리

--- front-end/src/redux/sagas/index.jsx
+++
@@ -1,7 +0,0 @@
-import { all, fork } from "redux-saga/effects";
-import AuthSaga from "./AuthSaga";
-import PostListSaga from "./PostListSaga";
-
-export default function* rootSaga() {
-  yield all([fork(AuthSaga), fork(PostListSaga)]);
-}
