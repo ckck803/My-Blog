f5147cc51b938f05ae38d565475fe94099fbf666
Dongwoo <ckck803@gmail.com>
2022-04-01 22:12:34 +0900

    JPA Auditing 을 위한 Config 추가

--- src/main/java/com/example/backend/config/JpaConfig.java
+++ src/main/java/com/example/backend/config/JpaConfig.java
@@ -1,9 +1,27 @@
 package com.example.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider(){
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String userName = "";
+            if(authentication!= null){
+                userName = authentication.getName();
+            }
+            return Optional.of(userName);
+        };
+    }
 }
