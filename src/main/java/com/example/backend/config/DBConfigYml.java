47a7d01a463fb22c227902d5fc283aec9d5ad0b9
Dongwoo <ckck803@gmail.com>
2022-04-04 01:23:54 +0900

    DB 설정 Rollback

--- src/main/java/com/example/backend/config/DBConfigYml.java
+++ src/main/java/com/example/backend/config/DBConfigYml.java
@@ -8,31 +8,31 @@ import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
-@Profile("prod")
-@PropertySource(value = "file:${user.home}/myblog/config/application-mysql.yml"
-        , factory = YamlPropertySourceFactory.class
-        , ignoreResourceNotFound = true)
+//@Profile("prod")
+//@PropertySource(value = "file:${user.home}/myblog/config/application-mysql.yml"
+//        , factory = YamlPropertySourceFactory.class
+//        , ignoreResourceNotFound = true)
 public class DBConfigYml {
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.hikari.username}")
-    private String username;
-
-    @Value("${spring.datasource.hikari.password}")
-    private String password;
-
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .driverClassName(driverClassName)
-                .url(url)
-                .username(username)
-                .password(password)
-                .build();
-    }
+//
+//    @Value("${spring.datasource.driver-class-name}")
+//    private String driverClassName;
+//
+//    @Value("${spring.datasource.url}")
+//    private String url;
+//
+//    @Value("${spring.datasource.hikari.username}")
+//    private String username;
+//
+//    @Value("${spring.datasource.hikari.password}")
+//    private String password;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        return DataSourceBuilder.create()
+//                .driverClassName(driverClassName)
+//                .url(url)
+//                .username(username)
+//                .password(password)
+//                .build();
+//    }
 }
